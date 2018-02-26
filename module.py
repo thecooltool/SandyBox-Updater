7ffa9c0099f88870661c7adcbade91209edbc0c9
Alexander Rössler <mail@roessler.systems>
2018-02-26 09:26:46 +0100

    update upstream Cetus and Machineface reference

--- module.py
+++ module.py
@@ -1090,8 +1090,8 @@ def main():
             aptOfflineInstallPackages('machinekit machinekit-dev machinekit-xenomai', force=True)  # force update of Machinekit
             updateHostGitRepo('thecooltool', 'AP-Hotspot', '~/bin/AP-Hotspot', ['sudo make install'])
             updateHostGitRepo('thecooltool', 'beaglebone-universal-io', '~/bin/beaglebone-universal-io', ['make', 'sudo make install'])
-            updateHostGitRepo('qtquickvcp', 'Cetus', '~/Cetus', [''])
-            updateHostGitRepo('qtquickvcp', 'Machineface', '~/Machineface', [''])
+            updateHostGitRepo('machinekit', 'Cetus', '~/Cetus', [''])
+            updateHostGitRepo('machinekit', 'Machineface', '~/Machineface', [''])
             updateHostGitRepo('thecooltool', 'mjpeg-streamer', '~/bin/mjpeg-streamer', ['make -C mjpg-streamer-experimental',
                                                                                         'sudo make -C mjpg-streamer-experimental install'])
             updateHostGitRepo('thecooltool', 'machinekit-configs', '~/machinekit-configs', [], branch='develop')
